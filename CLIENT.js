a786230f70ab9d2d11b6d697921935f8005f52c7
johan <none@none>
2018-03-01 13:21:54 +0000

    "Starting on the client code"

---
+++ CLIENT.js
@@ -0,0 +1,29 @@
+// This file and the CLIENT variable in all caps to indicate it is a global object/variable
+// if this was a module we where to export the CLIENT object
+var CLIENT = {};
+
+CLIENT.connect = function connect() {
+	var sock = new SockJS('http://johan.webide.se/_jsql/sockjs');
+	sock.onopen = function() {
+		console.log('open');
+		sock.send('test');
+	};
+	
+	sock.onmessage = function(e) {
+		console.log('message', e.data);
+		sock.close();
+	};
+	
+	sock.onclose = function() {
+		console.log('close');
+	};
+};
+
+CLIENT.api = function api(command, options, callback) {
+
+	
+	
+});
+
+
+
---
+++ example.js
@@ -0,0 +1,16 @@
+
+window.onload = function windowLoaded() {
+	CLIENT.connect();
+};
+
+var button = document.getElementById("button");
+button.onclick = function buttonClicked() {
+	
+	CLIENT.api("hello", {to: "world"}, function hello(err, resp) {
+		var message = document.getElementById("message");
+		if(err) return alert(err.message);
+		else message.innerText = resp.text;
+	});
+	
+};
+
